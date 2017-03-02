8869b92577a519ae2c1fcf9c4a86d9c7c58b5e98
Eugene Frys <eugene.frys@gmail.com>
2017-03-02 16:43:08 +0100

    geocode

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,6 +1,12 @@
 class DogsController < ApplicationController
   def index
     @dogs = Dog.all
+    @addresses = Address.all
+    @hash = Gmaps4rails.build_markers(@addresses) do |address, marker|
+      marker.lat address.latitude
+      marker.lng address.longitude
+      # marker.infowindow render_to_string(partial: "/flats/map_box", locals: { flat: flat })
+    end
   end
 
   def show
