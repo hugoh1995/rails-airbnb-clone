845d25dcd018e5526ef029bc90d6946fd2caa332
hugo houyez <hugohconcordia@gmail.com>
2017-03-05 16:02:25 +0100

    add delete to request

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -21,6 +21,12 @@ class RequestsController < ApplicationController
     end
   end
 
+  def destroy
+    request = Request.find(params[:id])
+    request.destroy
+    redirect_to pages_reservations_path
+  end
+
   private
 
   def request_params
