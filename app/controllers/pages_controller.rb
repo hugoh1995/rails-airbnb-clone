6694ccb7731c8b13806a4e7df2f79e77d1d28d9f
hugo houyez <hugohconcordia@gmail.com>
2017-03-07 10:11:02 +0100

    clean rooting to match current document

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
   #homepage
-  def home
+  def index
     @dogs = Dog.all
     @users = User.all
   end
